280e579a49633b950c539b9a731ccc0956f9dfd4
Sophearah Suy-Puth <sophearahspsp@gmail.com>
2023-11-18 15:11:07 -0500

    basic websocket interaction

--- frontend/src/components/HomeScreen.tsx
+++ frontend/src/components/HomeScreen.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 interface HomeScreenProps {
 	createCall: () => Promise<string>;
 	startHairCheck: (url: string) => Promise<void>; // Adjust the type based on your actual requirements
 }
 
+
+interface SocketMessage {
+    id: number;
+    text: string;
+}
+
 const HomeScreen = ({ createCall, startHairCheck }: HomeScreenProps) => {
+    
+    const [inputMessage, setInputMessage] = useState<string>('');
+    const [chatMessages, setChatMessages] = useState<SocketMessage[]>([]);
+    
+    useEffect(() => {
+        socket.on('chat message', (msg: string) => {
+            setChatMessages((prevMessages) => [
+                ...prevMessages,
+                { id: prevMessages.length, text: msg },
+            ]);
+        });
+      
+        // Clean up the socket connection when the component unmounts
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const handleSendSocketMessage = () => {
+        socket.emit('chat message', "socket message here");
+    };
+    
     const startDemo = () => {
 		createCall().then((url: string) => {
 			startHairCheck(url);
@@ -18,6 +49,9 @@ const HomeScreen = ({ createCall, startHairCheck }: HomeScreenProps) => {
             <p>Start the demo with a new unique room by clicking the button below.</p>
                 <button onClick={startDemo} type="button">
                 	Click to start a call
+                </button>
+                <button onClick={handleSendSocketMessage} type="button">
+                	send message
                 </button>
             <p>Select “Allow” to use your camera and mic for this call if prompted</p>
         </div>
