74a2e4c76e38242ef853afe952dc71548fa4c989
Sophearah Suy-Puth <sophearahspsp@gmail.com>
2023-11-19 02:58:48 -0500

    populate edit profile form, save changes to profile to supabase

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -8,12 +8,19 @@ import EditProfileModal from './components/Dashboard/EditProfileModal';
 
 const bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
+export interface Profile {
+    username: string;
+    bio: string;
+    user_id: number;
+}
+
 export interface User {
     id: number;
 }
 
 function Profile() {
     const [user, setUser] = useState<User | null>(null);
+    const [profile, setProfile] = useState<Profile | null>(null);
     const [posts, setPosts] = useState<Post[]>([]);
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
@@ -27,17 +34,31 @@ function Profile() {
 
                 setUser(d.user);
 
+                // Fetch user profile data
+                const { data: profileData, error: profileError } = await supabase
+                    .from('profiles')
+                    .select('user_id, username, bio')
+                    .eq('user_id', d.user.id)
+                    .single();
+                
+                if (profileError) {
+                    throw profileError;
+                }
+                
+                setProfile(profileData);
+
                 const { data: fetchedPosts, error } = await supabase
                     .from('posts')
                     .select('id, content, user_id')
                     .eq('user_id', d.user.id);
+
                 if (error) {
                     throw error;
                 }
 
                 setPosts(fetchedPosts);
             } catch (error) {
-                console.error('Error fetching posts', error);
+                console.error('Error fetching data', error);
             }
         };
 
@@ -68,8 +89,8 @@ function Profile() {
                             <EditIcon />
                         </IconButton>
                     </Stack>
-                    <Typography fontWeight="bold">{"username"}</Typography>
-                    <Typography>{bio}</Typography>
+                    <Typography fontWeight="bold">{profile?.username}</Typography>
+                    <Typography>{profile?.bio || bio}</Typography>
                 </Stack>
             )}
 
--- frontend/src/components/Dashboard/EditProfileModal.tsx
+++ frontend/src/components/Dashboard/EditProfileModal.tsx
@@ -1,12 +1,79 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Button, TextField, Typography, Box, Modal, Stack} from '@mui/material'
 import { supabase } from '../../supabaseClient'
+
+export interface Profile {
+    username: string;
+    bio: string;
+    user_id: number;
+}
+
+export interface User {
+    id: number;
+}
 
 function EditProfileModal(props: { open: boolean; handleClose: () => void }) {
     const { open, handleClose } = props;
 
     const [bio, setBio] = useState('');
     const [username, setUsername] = useState('');
+    const [user, setUser] = useState<User | null>(null);
+    const [profile, setProfile] = useState<Profile | null>(null);
+
+    useEffect(() => {
+        // Fetch user data and posts from Supabase
+        const fetchUserData = async () => {
+            try {
+                const { data: d, error: userError } = await supabase.auth.getUser();
+
+                setUser(d.user);
+
+                // Fetch user profile data
+                const { data: profileData, error: profileError } = await supabase
+                    .from('profiles')
+                    .select('user_id, username, bio')
+                    .eq('user_id', d.user.id)
+                    .single();
+                
+                if (profileError) {
+                    throw profileError;
+                }
+                
+                setProfile(profileData);
+                setUsername(profileData.username);
+                setBio(profileData.bio);
+            } catch (error) {
+                console.error('Error fetching data', error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    const handleSave = async () => {
+        try {
+            // Ensure user and profile data are available
+            if (!user || !profile) {
+                console.error('User or profile data is missing');
+                return;
+            }
+
+            // Update the profile data in the 'profiles' table
+            const { data, error } = await supabase
+                .from('profiles')
+                .update({ username, bio })
+                .eq('user_id', user.id);
+
+            if (error) {
+                throw error;
+            }
+
+            console.log('Profile updated successfully:', data);
+            handleClose(); // Close the modal after successful update
+        } catch (error) {
+            console.error('Error updating profile', error);
+        }
+    };
 
     return (
         <Modal open={open} onClose={handleClose}>
@@ -51,7 +118,7 @@ function EditProfileModal(props: { open: boolean; handleClose: () => void }) {
                     <Button variant="outlined" color="primary" onClick={() => console.log("cancel")}>
                         Cancel
                     </Button>
-                    <Button variant="contained" color="primary" onClick={() => console.log("save")}>
+                    <Button variant="contained" color="primary" onClick={handleSave}>
                         Save
                     </Button>
                 </Stack>
